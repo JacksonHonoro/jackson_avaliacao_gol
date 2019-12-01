1d19edbd24d45804160b0e96fe7dd02055afefed
JacksonHonoro <jackson.honoro@gmail.com>
2019-12-01 16:42:37 -0300

    config axios and hooks - page Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Background from '../../components/Background';
 
 import {Container, Form, FormInput, SubmitButton} from './styles';
 
 export default function Main() {
+  const [city, setCity] = useState('');
+
+  function handleSearch() {}
+
   return (
     <Background>
       <Container>
         <Form>
-          <FormInput placeholder="Pesquise por cidade" />
-          <SubmitButton onPress={() => {}}>Pesquisar</SubmitButton>
+          <FormInput
+            placeholder="Pesquise por cidade"
+            value={city}
+            onChange={e => setCity(e.target.value)}
+          />
+          <SubmitButton onPress={handleSearch}>Pesquisar</SubmitButton>
         </Form>
       </Container>
     </Background>
