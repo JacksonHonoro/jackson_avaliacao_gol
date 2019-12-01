10ca415886ec1e07795c5d28d298575841b3e47b
JacksonHonoro <jackson.honoro@gmail.com>
2019-11-30 22:09:32 -0300

    created components Input/Button

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import Weather from './pages/Weather';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      Weather,
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#084D6E',
+        },
+        headerTintColor: '#FFF',
+      },
+    }
+  )
+);
+
+export default Routes;
